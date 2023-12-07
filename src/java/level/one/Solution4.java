eb3a652dfc3ae5bdaedcf5b553190af807c78ffb
chan-hyeok-kim <daiolous@gmail.com>
2023-12-07 16:55:10 +0900

    coding trainig ex4

--- src/java/level/one/Solution4.java
+++ src/java/level/one/Solution4.java
@@ -2,7 +2,21 @@ package java.level.one;
 
 public class Solution4 {
 	public int[] solution(String s) {
-        int[] answer = {};
+        int[] answer = new int[s.length()];
+ 
+        answer[0]=-1;
+        for(int i=1; i<s.length(); i++) {
+        	for(int j=i-1; j>=0; j--) {
+        		if(s.charAt(i)==s.charAt(j)) {
+        			answer[i]=i-j;
+                    break;
+        		}else {
+        			answer[i]=-1;
+        		}
+        		
+        	}
+        }
+        
         return answer;
     }
 }
