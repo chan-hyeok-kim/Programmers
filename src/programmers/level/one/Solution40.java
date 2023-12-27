c662240842c84a1c4f5fe118d984c982a733a742
chan-hyeok-kim <daiolous@gmail.com>
2023-12-27 11:51:31 +0900

    coding training

--- src/programmers/level/one/Solution40.java
+++ src/programmers/level/one/Solution40.java
@@ -1,6 +1,33 @@
 package programmers.level.one;
 
 class Solution40 {
-	
+	public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String answer1 = "";
+        String answer2 = "";
+        
+        int count1=0;
+        int count2=0;
+        for(int i=0; i<goal.length; i++){
+            answer2+=goal[i];
+            if(count1<cards1.length && goal[i].equals(cards1[count1])){
+                answer1+=goal[i];
+                count1++;
+            }else if(count2<cards2.length && goal[i].equals(cards2[count2])){
+                answer1+=goal[i];
+                count2++;
+            }  
+        }
+       
+        if(answer1.equals(answer2)){
+            answer1="Yes";
+        }else{
+            answer1="No";
+        }
+        
+        
+            
+        return answer1;
+    }
 	
 }
+//카드 뭉치
--- src/programmers/level/one/Solution41.java
+++ src/programmers/level/one/Solution41.java
@@ -1,6 +1,40 @@
 package programmers.level.one;
 
+import java.util.*;
+
 class Solution41 {
-	
+	public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        
+        List<Integer> day=new ArrayList<Integer>();
+        
+        if(k>score.length){
+            for(int i=0; i<score.length; i++){
+                day.add(score[i]);
+                Collections.sort(day);
+                answer[i]=day.get(0);
+            }
+            return answer;  
+        }
+        for(int i=0; i<k; i++){
+            day.add(score[i]);
+            Collections.sort(day);
+            answer[i]=day.get(0);
+        }
+        
+        int count=0;
+        for(int i=k; i<score.length; i++){
+            if(score[i]>day.get(count)){
+                day.add(score[i]);        
+                count++;
+                Collections.sort(day);
+            }
+            answer[i]=day.get(count);
+        }
+        
+    
+        return answer;
+    }
 	
 }
+//명예의 전당(1)
