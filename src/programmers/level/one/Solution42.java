5de7789d0843ef1fff193cdef76d33f5557ba3b9
chan-hyeok-kim <daiolous@gmail.com>
2023-12-29 10:21:36 +0900

    coding training

--- src/programmers/level/one/Solution42.java
+++ src/programmers/level/one/Solution42.java
@@ -1,6 +1,21 @@
 package programmers.level.one;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
 class Solution42 {
-	
+	public String solution(int a, int b) {
+        String answer = "";
+        
+        LocalDate date=LocalDate.of(2016, a, b);
+        DayOfWeek dayOfWeek=date.getDayOfWeek();
+        answer=dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.US).toUpperCase();
+        
+        
+        return answer;
+    }
 	
 }
+//2016년
