e39a77ea9acb41274c529f7d69b6b39c6e735181
chan-hyeok-kim <daiolous@gmail.com>
2024-01-04 16:54:51 +0900

    coding training

---
+++ src/baekjoon/bronze/Main2557.java
@@ -0,0 +1,19 @@
+package baekjoon.bronze;
+
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+
+public class Main2557 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+        
+		bw.write("Hello World!");
+		bw.flush();
+		bw.close();
+	}
+
+}
--- src/programmers/level/one/Solution52.java
+++ src/programmers/level/one/Solution52.java
@@ -1,6 +1,47 @@
 package programmers.level.one;
 
+import java.util.List;
+
 class Solution52 {
-	
-	
+	 public int solution(String[] babbling) {
+	        int answer = 0;
+	        
+	        String[] start= {};
+	        List<String> list=List.of("aya","ye","woo","ma");
+		     
+	        for(String b: babbling) {
+		        for(int i=0; i<4; i++){
+		            if(b.startsWith(list.get(i))) {
+		            	if(b.length()==list.get(i).length()) {
+			            	  answer++;
+			            	  break;
+			            }
+		                b=b.substring(list.get(i).length());
+			               
+		                slice(list, b, i, answer);
+		                
+		            }
+		        }
+	        }
+	        
+	        
+	        return answer;
+	 }
+	 
+	 public void slice(List<String> list,String babble,int idx,int answer) {
+		                                             //이전 인덱스
+		   for(int i=0; i<4; i++){
+	             if(i!=idx && babble.startsWith(list.get(i))) {
+                   if(babble.length()==list.get(i).length()) {
+                	   answer++;
+                  	   break;
+                   }
+                   babble=babble.substring(list.get(i).length());
+                   slice(list, babble, idx, answer);;
+	             }else {
+	            	  break;
+	             }
+         }
+		 
+	 }
 }
