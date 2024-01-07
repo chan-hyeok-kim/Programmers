d8767efe6057f11a64ff642c7ab1fad03b203a81
chan-hyeok-kim <daiolous@gmail.com>
2024-01-07 16:04:08 +0900

    coding training

--- src/programmers/level/one/Solution57.java
+++ src/programmers/level/one/Solution57.java
@@ -1,6 +1,28 @@
 package programmers.level.one;
 
 class Solution57 {
-	
+	 public int solution(int n) {
+	        int answer = 0;
+	        
+	        int[] check=new int[100];
+	        
+	        int j=0;
+	        int i=1;
+	        while(j<check.length){
+	            String str=String.valueOf(i);
+	          
+	           if(i%3!=0 && !str.contains("3")){
+	              check[j]=i;
+	              j++;
+	            }
+	            i++;
+	        }
+	       
+	      
+	        answer=check[n-1];
+	        
+	        return answer;
+	    }
 	
 }
+//레벨0, 저주의 숫자3
