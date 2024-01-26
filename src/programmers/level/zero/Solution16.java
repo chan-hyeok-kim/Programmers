5b4c8b2fbee03e38109023a76b3b6a9b29f556bb
chan-hyeok-kim <daiolous@gmail.com>
2024-01-26 22:55:35 +0900

    coding training

--- src/programmers/level/zero/Solution16.java
+++ src/programmers/level/zero/Solution16.java
@@ -1,6 +1,29 @@
 package programmers.level.zero;
 
 public class Solution16 {
-	
+	public String[] solution(String[] str_list) {
+        String[] answer=new String[0];
+        
+        for(int i=0; i<str_list.length; i++){
+            if(str_list[i].equals("l")){
+               answer=new String[i]; 
+                
+               for(int j=0; j<i; j++){
+                   answer[j]=str_list[j];
+               } 
+               break;
+            }else if(str_list[i].equals("r")){
+               answer=new String[str_list.length-i-1]; 
+                
+               for(int j=i; j<str_list.length; j++){
+                   answer[j]=str_list[j];
+               } 
+                
+               break;
+            }
+        }
+        
+        return answer;
+    }
 }
-//
+//왼쪽 오른쪽
