15478a80b3245ae0a00622b2c9546cc9de86721c
chan-hyeok-kim <daiolous@gmail.com>
2024-02-01 21:05:11 +0900

    coding training

--- src/programmers/level/zero/Solution20.java
+++ src/programmers/level/zero/Solution20.java
@@ -1,6 +1,14 @@
 package programmers.level.zero;
 
 public class Solution20 {
-	
+	 public int[][] solution(int[][] arr) {
+	        int max = Math.max(arr.length, arr[0].length);
+	        int[][] array = new int[max][max];
+
+	        for (int i = 0; i < arr.length; i++) {
+	            System.arraycopy(arr[i], 0, array[i], 0, arr[0].length);
+	        }
+	        return array;
+	    }
 }
 //
--- src/programmers/level/zero/Solution21.java
+++ src/programmers/level/zero/Solution21.java
@@ -1,6 +1,23 @@
 package programmers.level.zero;
 
 public class Solution21 {
-	
+	public String[] solution(String[] picture, int k) {
+        String[] answer = new String[picture.length*k];
+        
+        for(int i=0; i<picture.length; i++){
+            String str="";
+            for(int j=0; j<picture[i].length(); j++){
+                for(int l=0; l<k; l++){
+                    str+=""+picture[i].charAt(j); 
+                }
+            }
+            
+            for(int j=0; j<k; j++){
+                answer[i*k+j]=str;
+            }
+        }
+        
+        return answer;
+    }
 }
-//
+//자바 11부터 추가된 String의 repeat()메서드 활용하면 좋을 듯
